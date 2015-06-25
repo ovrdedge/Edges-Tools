6c0371e5ea3bebc0f69aa67163c1782d01e203f3
OvrDEdge <ovrdedge@hotmail.com>
2015-06-25 17:37:58 -0500

    What does this do?

---
+++ CDP-Walker.py
@@ -0,0 +1,7 @@
+__author__ = 'jedge'
+##############################################################################
+# The purpose of this script is to generate a spreadsheet of Cisco devices   #
+# based on a seed ip address, username, and password. It has other settings  #
+# also that will be required to login
+
+seed_ipaddr = raw_input("Enter Seed Cisco Device")

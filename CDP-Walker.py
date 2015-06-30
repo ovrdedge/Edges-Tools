2ea4bb7a0a3643fec6990ae23e737fac9a9b6633
OvrDEdge <ovrdedge@hotmail.com>
2015-06-30 17:22:09 -0500

    I don't want to specify a message

--- CDP-Walker.py
+++ CDP-Walker.py
@@ -22,9 +22,15 @@ while loop_again:
 
 # Loading up the username / password variable.
 print "Please tell me all the passwords you want to try. I'll let you know how"
-print "many you've loaded starting with an index of '0'."
+print "many you've loaded starting with an index of '0'. For password only"
+print "credentials, please use a username of 'nouser followed by a number. For"
+print "example: 'nouser01'. lease make sure and tell me the credentials in the"
+print "order you want me to try them in. Finally, when your done, give me an"
+print "empty username and i'll stop looping."
+
 credential_count = 0
 credentials=[]
+all_done = False
 
 loop_again = True
 while loop_again:
@@ -33,17 +39,23 @@ while loop_again:
     loop_again_inner = True
     while loop_again_inner:
         username = raw_input("Enter User Name: ")
-        if edgesTools.is_sane_string(username):
-            loop_again_inner False
+        if username == '':
+            all_done = True
+        elif  edgesTools.is_sane_string(username):
+            loop_again_inner = False
         else:
             print "String to long, please try again."
+
+    # Breaking out of the outer while since we received an empty username
+    if all_done:
+        break
 
     # Prompting for Password, then validating input.
     loop_again_inner = True
     while loop_again_inner:
         password = raw_input("Enter Password: ")
         if edgesTools.is_sane_string(password):
-            loop_again_inner False
+            loop_again_inner = False
         else:
             print "String to long, please try again."
 
@@ -52,7 +64,13 @@ while loop_again:
         'password': password
     }
 
+    print "Credential %s loaded" % (credential_count)
     credential_count += 1
+
+# Now that the while loop is finished, i have to make sure they gave me some credentials.
+if credentials.len() == 0:
+    print "I don't see any valid credentials. Nothing to do. Have a nice day."
+    exit()
 
 # Prompting for output file, then validating input
 
